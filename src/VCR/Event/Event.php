fc9485072c307c85a57f4452c92ed5a62f12f662
Chris Gmyr <cmgmyr@gmail.com>
2020-06-24 09:42:53 -0400

    more pr updates

--- src/VCR/Event/Event.php
+++ src/VCR/Event/Event.php
@@ -2,13 +2,14 @@
 
 namespace VCR\Event;
 
-// inferior to Symfony/Contracts PHP minimal version
-if (class_exists(\Symfony\Contracts\EventDispatcher\Event::class)) {
-    class Event extends \Symfony\Contracts\EventDispatcher\Event
-    {
-    }
-} else {
+if (class_exists('Symfony\Component\EventDispatcher\Event')) {
     class Event extends \Symfony\Component\EventDispatcher\Event
     {
+        //
+    }
+} else {
+    class Event extends \Symfony\Contracts\EventDispatcher\Event
+    {
+        //
     }
 }
--- src/VCR/Videorecorder.php
+++ src/VCR/Videorecorder.php
@@ -99,9 +99,13 @@ class Videorecorder
      * @param Event $event The event to pass to the event handlers/listeners.
      * @return Event
      */
-    private function dispatch($eventName, Event $event = null)
+    private function dispatch(Event $event, $eventName = null)
     {
-        return $this->getEventDispatcher()->dispatch($event, $eventName);
+        if (class_exists('Symfony\Component\EventDispatcher\Event')) {
+            return $this->getEventDispatcher()->dispatch($event, $eventName);
+        }
+
+        return $this->getEventDispatcher()->dispatch($eventName, $event);
     }
 
     /**
@@ -218,14 +222,14 @@ class Videorecorder
         }
 
         $event = new BeforePlaybackEvent($request, $this->cassette);
-        $this->dispatch(VCREvents::VCR_BEFORE_PLAYBACK, $event);
+        $this->dispatch($event, VCREvents::VCR_BEFORE_PLAYBACK);
 
         $response = $this->cassette->playback($request);
 
         // Playback succeeded and the recorded response can be returned.
         if (!empty($response)) {
             $event = new AfterPlaybackEvent($request, $response, $this->cassette);
-            $this->dispatch(VCREvents::VCR_AFTER_PLAYBACK, $event);
+            $this->dispatch($event, VCREvents::VCR_AFTER_PLAYBACK);
             return $response;
         }
 
@@ -248,11 +252,11 @@ class Videorecorder
 
         $this->disableLibraryHooks();
 
-        $this->dispatch(VCREvents::VCR_BEFORE_HTTP_REQUEST, new BeforeHttpRequestEvent($request));
+        $this->dispatch(new BeforeHttpRequestEvent($request), VCREvents::VCR_BEFORE_HTTP_REQUEST);
         $response = $this->client->send($request);
-        $this->dispatch(VCREvents::VCR_AFTER_HTTP_REQUEST, new AfterHttpRequestEvent($request, $response));
+        $this->dispatch(new AfterHttpRequestEvent($request, $response), VCREvents::VCR_AFTER_HTTP_REQUEST);
 
-        $this->dispatch(VCREvents::VCR_BEFORE_RECORD, new BeforeRecordEvent($request, $response, $this->cassette));
+        $this->dispatch(new BeforeRecordEvent($request, $response, $this->cassette), VCREvents::VCR_BEFORE_RECORD);
         $this->cassette->record($request, $response);
         $this->enableLibraryHooks();
 
